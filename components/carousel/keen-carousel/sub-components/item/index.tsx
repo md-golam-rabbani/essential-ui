b208a56855d62c09fcd6bd431db3a66596614276
dev-rabbani <rabbani.cse.69@gmail.com>
2024-08-24 01:48:37 +0600

    refactor: Update `KeenCarouselItem` and update module css selector

--- components/carousel/keen-carousel/sub-components/item/index.tsx
+++ components/carousel/keen-carousel/sub-components/item/index.tsx
@@ -1,11 +1,24 @@
 import { cn } from '@/lib/shadcn/utils';
 import { IKeenCarouselItem } from './interface';
 
+/**
+ * Component for rendering a Keen Carousel item.
+ */
 export function KeenCarouselItem({ children, className }: IKeenCarouselItem) {
   return (
     <div
       className={cn(
-        'keen-slider__slide keen-carousel-slide !overflow-visible',
+        // Required Keen slider class
+        'keen-slider__slide',
+
+        // Override default Keen slider styles
+        '!overflow-visible',
+
+        // Custom styles for the carousel item
+        // Note: Check keen-carousel.module.css before renaming
+        'keen-carousel-slide',
+
+        // Additional custom class names
         className
       )}
       aria-label="Keen slider slide"
