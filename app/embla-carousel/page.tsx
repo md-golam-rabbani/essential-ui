e3b83ef3f8f0a89a5a3b87abfe2b7a8eb5e1786b
dev-rabbani <rabbani.cse.69@gmail.com>
2024-09-04 23:20:40 +0600

    feat: Create example page

---
+++ app/embla-carousel/page.tsx
@@ -0,0 +1,9 @@
+import { EmblaCarousel } from '@/components/carousel/keen-carousel/embla-carousel';
+
+export default function Page() {
+  return (
+    <div>
+      <EmblaCarousel />
+    </div>
+  );
+}
