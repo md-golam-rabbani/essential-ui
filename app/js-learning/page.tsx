9800f24bfd6be1110a9500c65dfa4285e32ce8aa
dev-rabbani <rabbani.cse.69@gmail.com>
2024-11-16 10:59:41 +0600

    refactor: Add basic page

---
+++ app/js-learning/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@/components/typography';
+
+export default function Page() {
+  return (
+    <div className="bg-slate-100 py-20">
+      <div className="container">
+        <Typography size="h1">Js Learning</Typography>
+      </div>
+    </div>
+  );
+}
