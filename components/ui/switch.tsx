1bf5b449f147bb83cffb6e78e26f0ed56d2ca950
golam.rabbani <golam.rabbani@technonext.com>
2025-06-16 18:29:28 +0600

    refactor: Removed duplicate classes

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -13,7 +13,7 @@ function Switch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        'peer inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-slate-200 border-transparent shadow-xs transition-all outline-none focus-visible:border-slate-950 focus-visible:ring-[3px] focus-visible:ring-slate-950/50 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=unchecked]:bg-slate-200 dark:border-slate-800 dark:focus-visible:border-slate-300 dark:focus-visible:ring-slate-300/50 dark:data-[state=checked]:bg-slate-50 dark:dark:data-[state=unchecked]:bg-slate-800/80 dark:data-[state=unchecked]:bg-slate-200/80 dark:data-[state=unchecked]:bg-slate-800',
+        'peer inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:border-slate-950 focus-visible:ring-[3px] focus-visible:ring-slate-950/50 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=unchecked]:bg-slate-200 dark:border-slate-800 dark:focus-visible:border-slate-300 dark:focus-visible:ring-slate-300/50 dark:data-[state=checked]:bg-slate-50 dark:dark:data-[state=unchecked]:bg-slate-800/80 dark:data-[state=unchecked]:bg-slate-800',
         className
       )}
       {...props}
